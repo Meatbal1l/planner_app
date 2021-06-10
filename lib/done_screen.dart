2d99a0215ebcfd08cce01d22d7879651f07b21e0
Meatbal1l <pavel070900@gmail.com>
2021-06-10 20:08:32 +0300

    all files

---
+++ lib/done_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FinishedPlans extends StatefulWidget {
+  const FinishedPlans({Key? key}) : super(key: key);
+
+  @override
+  _FinishedPlansState createState() => _FinishedPlansState();
+}
+
+class _FinishedPlansState extends State<FinishedPlans> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
